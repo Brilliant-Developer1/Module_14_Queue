cf37e44387456acd568d09e2b9c469fa69a496b5
Brilliant-Developer1 <email2obail@gmail.com>
2023-07-04 22:01:51 +0600

    file added

---
+++ queue_list.cpp
@@ -0,0 +1,110 @@
+// #include<bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <strstream>
+#include <tuple>
+#include <typeinfo>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+
+class myQueue
+{
+public:
+    list<int> l;
+    void push(int value)
+    {
+
+        l.push_back(value);
+    }
+    void pop()
+    {
+
+        l.pop_front();
+    }
+
+    int front()
+    {
+        return l.front();
+    }
+    int size()
+    {
+        return l.size();
+    }
+    bool empty()
+    {
+        return l.empty();
+    }
+};
+int main()
+{
+    myQueue que;
+    int input;
+    cin >> input;
+    for (int i = 0; i < input; i++)
+    {
+        int x;
+        cin >> x;
+        que.push(x);
+    }
+
+    while (!que.empty())
+    {
+        cout << que.front() << endl;
+        que.pop();
+    }
+
+    return 0;
+}
